60ceaeedc9578b36814af8760278d2d4aff5d828
Levi <kluaythong@gmail.com>
2021-05-12 17:03:15 -0500

    fixed visual bugs

--- Assets/Scripts/CharacterScripts/PlayerController.cs
+++ Assets/Scripts/CharacterScripts/PlayerController.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class PlayerController : NetworkBehaviour
 {
-    public Vector3 leftRacketSpawn = new Vector3(-10, 0, 0);
-    public Vector3 rightRacketSpawn = new Vector3(10, 0, 0);
+    public Vector3 leftRacketSpawn = new Vector3(-10.5f, 0.5f, 0);
+    public Vector3 rightRacketSpawn = new Vector3(10.5f, 0.5f, 0);
     public float moveSpeed;
     //public VectorValue startingPosition;
 
@@ -27,6 +27,7 @@ public class PlayerController : NetworkBehaviour
     public Transform attackPoint;
     public float attackRange = 1.0f;
     public LayerMask enemyLayers;
+    public LayerMask collision;
 
     private int numDeaths = 0;
 
@@ -116,12 +117,9 @@ public class PlayerController : NetworkBehaviour
                 //find the next target position when a player attempts to move
                 if (movement != Vector2.zero)
                 {
-                    //Debug.Log("Movement X: " + movement.x);
                     animator.SetFloat("MoveX", movement.x);
                     animator.SetFloat("MoveY", movement.y);
 
-                    //makes sure an area is walkable before allowing a player move
-                    //StartCoroutine(character.Move(movement, OnMoveOver));
                     var targetPos = transform.position;
                     targetPos.x += movement.x;
                     targetPos.y += movement.y;
@@ -165,10 +163,6 @@ public class PlayerController : NetworkBehaviour
         //respawn
     }
 
-    private void DoDamage()
-    {
-    }
-
     IEnumerator Move(Vector3 targetPos)
     {
         isMoving = true;
@@ -188,7 +182,7 @@ public class PlayerController : NetworkBehaviour
         var difference = targetPosition - transform.position;
         var direction = difference.normalized;
 
-        if (Physics2D.OverlapCircle(targetPosition, 0.3f, enemyLayers) != null)
+        if (Physics2D.OverlapCircle(targetPosition, 0.3f, enemyLayers | collision) != null)
         {
             return false;
         }
